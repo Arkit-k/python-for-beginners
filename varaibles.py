901c7f649f2a979ec1ae91abb9a55d4125aac2b0
arkit_k <arkitkarmokar007@gmail.com>
2025-03-10 21:58:02 +0530

    10-03

---
+++ varaibles.py
@@ -0,0 +1,20 @@
+message = "hello world python"
+print(message)
+message = " Hello python "
+print(message)
+
+name = "ada lovelace"
+#this will make the titile a and l capital 
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+#concatinating strings 
+
+first_name = "ada"
+second_name = "loveable"
+final_name = first_name + " " + second_name
+
+print(final_name)
+
+#This method of combining strings is called concatenation.
