39d0e8eede16b66d53d527a286dac3928d5e6d4c
arkit_k <arkitkarmokar007@gmail.com>
2025-03-23 22:07:43 +0530

    final commit

--- if-else-p1.py
+++ if-else-p1.py
@@ -20,3 +20,30 @@ for  color in alien_color:
       if color == congo:
             print("10 points added")
 
+if alien_color == 'green':
+      print('color is green')
+elif alien_color == 'yellow':
+      print('color is yellow')
+elif alien_color == 'red':
+      print('color is red')
+else:
+      print('not found match')
+
+
+
+
+age = int(input('enter your age'))
+if age <= 2:
+      print(" oyee cute baby")
+elif age >= 2 and age <= 4:
+      print("he is a little champ ")
+elif age >= 4 and age <=  13:
+      print("yeo bacha bada horahai")
+elif age >= 13 and age <= 20:
+      print("bhai tenager bangaya")
+elif age >= 20 and age <= 30:
+      print("girlfriend banale bhai")
+elif age >= 30 and age <= 60:
+      print('elder')
+else :
+      print('atleast give me a valid input')
