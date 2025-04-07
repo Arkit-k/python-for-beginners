5b377bd23f6af0457dc98161b55cfcf13119cbcc
arkit_k <arkitkarmokar007@gmail.com>
2025-04-07 22:18:19 +0530

    latest commit

--- function.py
+++ function.py
@@ -17,3 +17,28 @@ def favorite_book(book):
       print(f' my favorite books is {book}')
 
 favorite_book('girl with the dragon tattoo')
+
+# positional argument
+def describe_pet(animal_type, pet_name):
+      print(f"\n my breed of animal is {animal_type} and pet name is {pet_name}")
+describe_pet('hamster' , 'harry')
+
+def Tshirt(size='l' , printed='i love python'):
+      print(f"my size is {size} and you have to print {printed} in the tshirt ")
+Tshirt(22 , ' i am tyler derdn')
+
+def get_formatted_name(first_name , last_name):
+      full_name = first_name + ' ' + last_name
+      return full_name.title()
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+     if middle_name:
+          full_name = first_name + ' ' + middle_name + ' ' + last_name
+     else:
+          full_name = first_name + ' ' + last_name
+          return full_name.title()
+musiciane = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musiciane = get_formatted_name('john', 'hooker', 'lee')
